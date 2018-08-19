957267a3c9b3c7c41b7fcf4630dcaaa6460ed9fd
Michael Dayah <michael@dayah.com>
2018-08-18 21:20:26 -0400

    2005-08

---
+++ advertise-to-students.php
@@ -0,0 +1,46 @@
+<html>
+ <head>
+  <title>Advertise to Students on this Periodic Table site</title>
+  <style>
+   H1, H2 {  border-bottom: thin solid black; }
+   H1 { letter-spacing: -0.05em; font-family: Verdana, sans-serif; }
+   H2 { letter-spacing: 0.05em; font-family: sans-serif; margin-left: 2em; }
+   P { margin-left: 5em; }
+  </style>
+ </head>
+ <body>
+  <h1>Advertise to high school and college students</h1>
+
+<br />
+<b>Notice</b>:  Undefined index:  HTTP_REFERER in <b>/var/www/html/periodic/advertise-to-students.php</b> on line <b>14</b><br />
+  <h2>How did you find this page?</h2>
+  <p>Was it a search engine? How did an individual with a periodic table manage to snag such a high position on your search against multinational advertising agencies with millions in profit? Huge marketing campaigns? Thousands spent hiring "optimization" consultants? No, and for the same reason you found me, you'll want to advertise with me. You found this page because it contains clear, meaningful content. That's all people want on the web and that's all I want to provide.</p>
+
+  <p>Internet banner advertising is a mess. When my periodic table first went live in 1997, web banner advertising was not a few pennies per click. It was often a penny or more per <strong>impression</strong>. And for good reason: an agency knew its audience and a publisher only provided one spot to one agency. It was a relationship.</p>
+
+  <h2>What went wrong?</h2>
+  <p>Large middleman ad agencies and an overabundance of publishers. If Google AdSense, Burst, and Fastclick can promise advertisers your site will be advertised on sites that contain your keywords while simultaneously promising publishers that if they have sites with your keywords, they will make money, what do you think will happen? Publishers will churn out sites with your keywords and put up a dozen banners. You have no control over it and half your money is going to the agency rather than the publisher.</p>
+  <p><strong>If you are trying to reach students by guessing keywords like "homework" for your AdSense campaign, you may want to reconsider whether it is money well spent.</strong></p>
+
+  <h2>What do you propose?</h2>
+  <p>I would like to develop an old-fashioned one-on-one relationship with one advertiser. I've developed web sites for a decade, including the most popular Lacey Chabert fan site, never placing a single banner on it in its five-year lifetime. I don't want to defile my web sites with unrelated ugly flashing banners. I will accept one leaderboard banner from one advertiser to run full time or multiple demographically targeted banners. On any given visit to the periodic table, <strong>only one</strong> image will be shown.</p>
+
+  <h2>How do I know I'm reaching students?</h2>
+  <p>Because I will give you access to the site's traffic and you will see that traffic 1) drops in half on Fridays and Saturdays, 2) drops to nearly zero the week of Christmas, 3) surges to six times its average at the beginning of the fall and spring semesters. Additionally, correcting for geography, these students are doing their homework at the same hours I did mine in high school and college.</p>
+  <p><strong>I honestly can think of no better way to precisely reach high school and college students than through a resource they will need to do their homework for a class required by every high school and college in the world.</strong></p>
+
+  <h2>How many students can I reach?</h2>
+  <p>Expect at minimum (except for around Christmas and at the beginning of summer) one thousand views of your message per day. When the fall semester starts, <strong>expect nearly 10,000 views on weekdays</strong> and slightly fewer mid-spring.</p>
+
+  <h2>Can I target a specific demographic of students?</h2>
+  <p>Absolutely. It's trivial to ensure any number of conditions are met. For example, would you like to pay a premium to ensure only unique visits are counted? Since this site is only one page, almost 70% of visits are unique and the remainder are either repeat visitors or those who've saved the site to their computer. How about only targeting Australian students? How about only showing your message to lazy students who are doing their homework after midnight their local time? <strong>Perhaps you only want to reach college students living in a dorm?</strong> Perhaps only students at Stanford? You can even combine restrictions, such as only British students who found this page through Google and are visiting Sunday night.</p>
+
+  <h2>How much?</h2>
+  <p>Paying for "clicks" rather than impressions evolved because advertisers didn't really know where their message was being shown or to whom. They could only be sure it worked if someone actually came to their site. This is not how highly directed, quality advertising works. Advertising with me will be paid for by impressions only. If you don't have a message to get across or image to present and just want to redirect as many people as possible to your site, perhaps we are not right for each other.</p>
+
+  <p>The specific CPM (cost per thousand impressions) is negotiable. Ask yourself how many pennies it's worth to your company for each student to see your message. Compare the price to taking out an ad in a college newspaper for a day. At a huge college, maybe 10,000 people will see your message&hellip;for how much? I am a web developer, not a businessman or an ad agency; <strong>offer me what you believe to be a fair rate for your advertising campaign and show me you are an honorable company and I will accept it.</strong></p>
+
+  <p><a href="feedback.html">Make a proposal</a> or <a href="./">finish your homework</a>.</p>
+
+ </body>
+</html>
