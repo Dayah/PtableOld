8713770b8e3c50831712f05b4e1a1a46d937a9b2
Michael Dayah <michael@dayah.com>
2018-08-20 23:03:02 -0400

    2007-11

--- advertise-to-students.php
+++ advertise-to-students.php
@@ -1,6 +1,6 @@
 <html>
  <head>
-  <title>Advertise to College Students on this Periodic Table site</title>
+  <title>Advertise to College Students on this Periodic Table</title>
   <style>
    H1, H2 {  border-bottom: thin solid black; }
    H1 { letter-spacing: -0.05em; font-family: Verdana, sans-serif; }
@@ -20,11 +20,11 @@
   <p><strong>If you are trying to reach students by guessing keywords like "homework" for your AdWords campaign, you may want to reconsider whether it is money well spent.</strong></p>
 
   <h2>What do you propose?</h2>
-  <p>I would like to develop an old-fashioned one-on-one relationship with one advertiser. I've developed web sites for a decade, including the most popular Lacey Chabert site, never placing a single banner on it in its five-year lifetime. I don't want to defile my web sites with unrelated ugly flashing banners. I will accept one leaderboard banner from one advertiser to run full time or multiple demographically targeted banners. On any given visit to the periodic table, <strong>only one</strong> banner will be shown.</p>
+  <p>I would like to develop an old-fashioned one-on-one relationship with one advertiser. I've developed web sites for a decade. I don't want to defile my web sites with unrelated ugly flashing banners. I will accept one leaderboard banner from one advertiser to run full time or multiple demographically targeted banners. On any given visit to the periodic table, <strong>only one</strong> banner will be shown.</p>
   <p>The greatest advantage of this one-on-one relationship is that we can craft a <strong>banner that appears to be part of the site</strong>, virtually guaranteeing it will not be overlooked. Imagine a visitor's shock to find that <strong>the advertisement not only knows where they are, but why they're visiting.</strong></p>
 
   <h2>How do I know I'm reaching students?</h2>
-  <p>Because I will give you access to the site's traffic statistics and you will see that traffic 1) drops in half on Fridays and Saturdays, 2) drops to nearly zero the week of Christmas, 3) surges to six times its average at the beginning of the fall and spring semesters. Additionally, correcting for geography, these students are doing their homework at the same hours I did mine in high school and college.</p>
+  <p>I will give you access to the site's traffic statistics and you will see that traffic 1) drops in half on Fridays and Saturdays, 2) drops to nearly zero the week of Christmas, 3) surges to six times its average at the beginning of the fall and spring semesters. Additionally, correcting for geography, these students are doing their homework at the same hours I did mine in high school and college. In fact, 1 in 12 visits originate from an American college campus, such as labs or dorms.</p>
   <p><strong>I honestly can think of no better way to precisely reach high school and college students than through a resource they will need to do their homework for a class required by every high school and college in the world.</strong></p>
 
   <h2>How many students can I reach?</h2>
@@ -34,9 +34,9 @@
   <p>Absolutely. It's trivial to ensure any number of conditions are met. For example, would you like to pay a premium to ensure only unique visits are counted? Since this site is only one page, almost 70% of visits are unique and the remainder are either repeat visitors or those who've saved the site to their computer. How about only targeting Australian students? How about only showing your message to lazy students who are doing their homework after midnight their local time? <strong>Perhaps you only want to reach college students living in a dorm?</strong> Perhaps only students at Stanford? You can even combine restrictions, such as only British students who found this page through Google and are visiting Sunday night.</p>
 
   <h2>How much?</h2>
-  <p>Paying for "clicks" rather than impressions evolved because advertisers didn't really know where their message was being shown or to whom. They could only be sure it worked if someone actually came to their site. This is not how highly directed, quality advertising works. Advertising with me will be paid for by impressions only. Banners are lucky to get half a percent clickthrough. My clickthrough ratio is close to 2%. A highly relevant text ad gets close to 4%.</p>
+<!--  <p>Paying for "clicks" rather than impressions evolved because advertisers didn't really know where their message was being shown or to whom. They could only be sure it worked if someone actually came to their site. This is not how highly directed, quality advertising works. Advertising with me will be paid for by impressions only. Banners are lucky to get half a percent clickthrough. My clickthrough ratio is close to 2%. A highly relevant text ad gets close to 4%.</p> -->
 
-  <p>The specific CPM (cost per thousand impressions) is negotiable, though rates start at $5. Ask yourself how many pennies it's worth to your company for each student to see your message. Compare the price to taking out an ad in a college newspaper for a day. At a huge college, maybe 10,000 people will see your message&hellip;for how much? I am a web developer, not a businessman or an ad agency; <strong>offer me what you believe to be a fair rate for your advertising campaign and show me you are an honorable company and I will accept it.</strong></p>
+  <p>The specific CPM (cost per thousand impressions) or CPC (cost per click) is negotiable, though rates start at <strong>$5 CPM or $0.50 CPC</strong> depending on whether you're running a branding or sales campaign respectively. Compare the price to taking out an ad in a college newspaper for a day. At a huge college, maybe 10,000 people will see your message&hellip;for how much? I am a web developer, not a businessman or an ad agency; <strong>offer me what you believe to be a fair rate for your advertising campaign and show me you are an honorable company and I will accept it.</strong></p>
 
   <p><a href="feedback.html">Make a proposal</a> or <a href="/periodic/" title="periodic table">finish your homework</a>.</p>
 
