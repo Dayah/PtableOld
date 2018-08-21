2acf3348d36aaee3a3a2b8b281529770f78e137c
Michael Dayah <michael@dayah.com>
2018-08-20 22:31:28 -0400

    2006-08

--- advertise-to-students.php
+++ advertise-to-students.php
@@ -28,7 +28,7 @@
   <p><strong>I honestly can think of no better way to precisely reach high school and college students than through a resource they will need to do their homework for a class required by every high school and college in the world.</strong></p>
 
   <h2>How many students can I reach?</h2>
-  <p>Expect at minimum (except for around Christmas and at the beginning of summer) five thousand views of your message per day. In the peak of the fall and spring semesters, <strong>expect 25,000 views on weekdays</strong>.</p>
+  <p>Expect at minimum (except for around Christmas and at the beginning of summer) five thousand views of your message per day. In the peak of the fall and spring semesters, <strong>expect 30,000 views on weekdays</strong>.</p>
 
   <h2>Can I target a specific demographic of students?</h2>
   <p>Absolutely. It's trivial to ensure any number of conditions are met. For example, would you like to pay a premium to ensure only unique visits are counted? Since this site is only one page, almost 70% of visits are unique and the remainder are either repeat visitors or those who've saved the site to their computer. How about only targeting Australian students? How about only showing your message to lazy students who are doing their homework after midnight their local time? <strong>Perhaps you only want to reach college students living in a dorm?</strong> Perhaps only students at Stanford? You can even combine restrictions, such as only British students who found this page through Google and are visiting Sunday night.</p>
@@ -36,7 +36,7 @@
   <h2>How much?</h2>
   <p>Paying for "clicks" rather than impressions evolved because advertisers didn't really know where their message was being shown or to whom. They could only be sure it worked if someone actually came to their site. This is not how highly directed, quality advertising works. Advertising with me will be paid for by impressions only. Banners are lucky to get half a percent clickthrough. My clickthrough ratio is close to 2%. A highly relevant text ad gets close to 4%.</p>
 
-  <p>The specific CPM (cost per thousand impressions) is negotiable, though rates start at $10. Ask yourself how many pennies it's worth to your company for each student to see your message. Compare the price to taking out an ad in a college newspaper for a day. At a huge college, maybe 10,000 people will see your message&hellip;for how much? I am a web developer, not a businessman or an ad agency; <strong>offer me what you believe to be a fair rate for your advertising campaign and show me you are an honorable company and I will accept it.</strong></p>
+  <p>The specific CPM (cost per thousand impressions) is negotiable, though rates start at $5. Ask yourself how many pennies it's worth to your company for each student to see your message. Compare the price to taking out an ad in a college newspaper for a day. At a huge college, maybe 10,000 people will see your message&hellip;for how much? I am a web developer, not a businessman or an ad agency; <strong>offer me what you believe to be a fair rate for your advertising campaign and show me you are an honorable company and I will accept it.</strong></p>
 
   <p><a href="feedback.html">Make a proposal</a> or <a href="./">finish your homework</a>.</p>
 
