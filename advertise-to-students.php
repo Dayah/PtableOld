ffc12961716cd2a1293755701c540c22394e4b4e
Michael Dayah <michael@dayah.com>
2018-08-20 22:33:35 -0400

    2006-10

--- advertise-to-students.php
+++ advertise-to-students.php
@@ -24,11 +24,11 @@
   <p>The greatest advantage of this one-on-one relationship is that we can craft a <strong>banner that appears to be part of the site</strong>, virtually guaranteeing it will not be overlooked. Imagine a visitor's shock to find that <strong>the advertisement not only knows where they are, but why they're visiting.</strong></p>
 
   <h2>How do I know I'm reaching students?</h2>
-  <p>Because I will give you access to the site's traffic and you will see that traffic 1) drops in half on Fridays and Saturdays, 2) drops to nearly zero the week of Christmas, 3) surges to six times its average at the beginning of the fall and spring semesters. Additionally, correcting for geography, these students are doing their homework at the same hours I did mine in high school and college.</p>
+  <p>Because I will give you access to the site's traffic statistics and you will see that traffic 1) drops in half on Fridays and Saturdays, 2) drops to nearly zero the week of Christmas, 3) surges to six times its average at the beginning of the fall and spring semesters. Additionally, correcting for geography, these students are doing their homework at the same hours I did mine in high school and college.</p>
   <p><strong>I honestly can think of no better way to precisely reach high school and college students than through a resource they will need to do their homework for a class required by every high school and college in the world.</strong></p>
 
   <h2>How many students can I reach?</h2>
-  <p>Expect at minimum (except for around Christmas and at the beginning of summer) five thousand views of your message per day. In the peak of the fall and spring semesters, <strong>expect 30,000 views on weekdays</strong>.</p>
+  <p>Expect at minimum (except for around Christmas and at the beginning of summer) ten thousand views of your message per day. In the peak of the fall and spring semesters, <strong>expect 50,000 views on weekdays</strong>.</p>
 
   <h2>Can I target a specific demographic of students?</h2>
   <p>Absolutely. It's trivial to ensure any number of conditions are met. For example, would you like to pay a premium to ensure only unique visits are counted? Since this site is only one page, almost 70% of visits are unique and the remainder are either repeat visitors or those who've saved the site to their computer. How about only targeting Australian students? How about only showing your message to lazy students who are doing their homework after midnight their local time? <strong>Perhaps you only want to reach college students living in a dorm?</strong> Perhaps only students at Stanford? You can even combine restrictions, such as only British students who found this page through Google and are visiting Sunday night.</p>
